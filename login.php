e075048fae98077e209089e0558ce044d0b6dc67
BintangAkbar15 <bintangakbar154@gmail.com>
2024-09-13 16:34:51 +0700

    idk but i think i forgot to commit

--- login.php
+++ login.php
@@ -21,11 +21,11 @@ include "controllers/controllerData.php";
                             <form action="" method="post">
                                 <div class="form-group">
                                     <label for="username">Username</label>
-                                    <input type="text" name="username" id="username" class="form-control">
+                                    <input type="text" name="username" id="username" class="form-control" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="password">Password</label>
-                                    <input type="password" name="password" id="password" class="form-control">
+                                    <input type="password" name="password" id="password" class="form-control" required>
                                 </div>
                                 <div class="form-group mt-2">
                                     <button type="submit" name="login" class="btn btn-primary">Login</button>
